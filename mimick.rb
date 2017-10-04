f3f4bf76b97853d596a20347aa32b12a9752a1e6
zeled3813 <zeled3813@hstat.org>
2017-10-04 15:37:12 +0000

    playing around with mimick.rb

--- mimick.rb
+++ mimick.rb
@@ -1,6 +1,10 @@
-def me_too(name)
-    puts "#{name.upcase}??? That's my name too!!!"
+def me_too(name,age,gender)
+    puts "#{name.upcase}??? That's my name too!!! #{age}??? That's my age too!!! #{gender}??? That's my gender too!!!"
 end
 puts "Hi, welcome to the terminal. Please enter your name:"
 name = gets.chomp
-me_too(name)
+puts "How old are you?"
+age = gets.chomp
+puts "Male or Female?"
+gender = gets.chomp
+me_too(name,age,gender)
